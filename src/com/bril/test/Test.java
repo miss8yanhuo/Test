5891631774cc2c0eb9a9f225c7ce18eb2bcd357d
miss8yanhuo <yangyong@bril.com.cn>
2018-08-08 17:28:18 +0800

    测试

--- src/com/bril/test/Test.java
+++ src/com/bril/test/Test.java
@@ -1,10 +1,10 @@
 package com.bril.test;
 
 public class Test {
-
+	static Boolean strs;
 	public static void main(String[] args) {
-		System.out.println(111122222);
-
+		System.out.println(111122222); 
+		System.out.println(strs);
 	}
 
 }
